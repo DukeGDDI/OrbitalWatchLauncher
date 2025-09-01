22cdaccc92fc599dae7033859cb14eb261628b65
RA9R <r@a9r.me>
2025-09-01 15:45:56 -0400

    switch to tar

--- scripts/zip-pdx.ps1
+++ scripts/zip-pdx.ps1
@@ -5,7 +5,7 @@ $name = (Get-Content source/pdxinfo | Select-String "^name=").ToString().Split("
 
 # Build paths
 $src = "builds/$name.pdx"
-$dst = "builds/$name.zip"
+$dst = "builds/$name.pdx.zip"
 
 # Ensure source exists
 if (-not (Test-Path $src)) {
@@ -18,6 +18,13 @@ if (Test-Path $dst) {
 }
 
 # Create archive
-Compress-Archive -Path $src -DestinationPath $dst -Force
+#Compress-Archive -Path $src -DestinationPath $dst -Force
+
+# Create archive with tar
+# -a : choose format based on extension (.zip → zip format)
+# -c : create new archive
+# -f : specify archive file
+# -C : change directory so that $src folder is top-level in the archive
+tar -a -cf $dst -C (Split-Path $src -Parent) (Split-Path $src -Leaf)
 
 Write-Output "Created: $dst"
